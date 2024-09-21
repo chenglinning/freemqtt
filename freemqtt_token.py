63652bfbb642dbb11691334f585ae88ab185662e
ningchenglin <ben-ning@163.com>
2024-09-22 07:38:08 +0800

    remove some test programs to miscellanceous path

--- freemqtt_token.py
+++ freemqtt_token.py
@@ -3,7 +3,6 @@ import uuid
 from freemqtt.server.tokentools import signToken, verifyToken
 # appid = uuid.uuid1().hex[0:8]
 # appname = 'demo'
-appname = '家悦'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("appid", help="freemqtt appid")
@@ -13,9 +12,8 @@ if __name__ == "__main__":
     data = verifyToken(token)
     print (f"\nAppID: {data}")
     print (f"Token: {token}")
-
+    
 # for guojie_app 
 # hx5hMRJ8z+5guo7qh6RNZPsw3quEOT+n50lyGCZg09A=
-        
 # demo
 # wNMAP5SRt53QkEf47mLrbBVDqcRtiuHlLAUMQxSPG1E=
